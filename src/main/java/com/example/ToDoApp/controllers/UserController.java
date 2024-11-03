2b39f4b31ce860102c4dd80802490aa8674f077b
dilusha07 <dilushasandaruwani07@gmail.com>
2024-11-03 17:45:44 +0530

    Add a user controller for retrieving all user objects

---
+++ src/main/java/com/example/ToDoApp/controllers/UserController.java
@@ -0,0 +1,34 @@
+package com.example.ToDoApp.controllers;
+
+import com.example.ToDoApp.model.User;
+import com.example.ToDoApp.services.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("api/v1/users")
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //Get current user
+    @GetMapping("/me")
+    public ResponseEntity<User> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentUser = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(currentUser);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> allUsers() {
+        List <User> users = userService.allUsers();
+        return ResponseEntity.ok(users);
+    }
+}
