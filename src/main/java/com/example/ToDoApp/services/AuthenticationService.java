343268f08ae1edd09d83bb5ea3c9fbf3a7ed74ae
dilusha07 <dilushasandaruwani07@gmail.com>
2024-11-03 17:42:02 +0530

    Implement helper functions for JWT token validation & Add security configuration file

---
+++ src/main/java/com/example/ToDoApp/services/AuthenticationService.java
@@ -0,0 +1,43 @@
+package com.example.ToDoApp.services;
+
+import com.example.ToDoApp.dto.LoginUserDto;
+import com.example.ToDoApp.dto.RegisterUserDto;
+import com.example.ToDoApp.model.User;
+import com.example.ToDoApp.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public User signup(RegisterUserDto input) {
+        User user = new User(input.getUsername(), input.getEmail(), passwordEncoder.encode(input.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDto input) {
+        User user = userRepository.findByEmail(input.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return user;
+    }
+
+}
